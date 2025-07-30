8564dcb66e0b018602141806857bbaaf8d70f268
Jose David Oliva <jdavidolivamuralles@gmail.com>
2025-07-30 01:26:23 -0600

    Third commit

--- proyectoAnalasis/src/main/java/com/umg/proyectoAnalasis/controller/JwtAuthenticationController.java
+++
@@ -1,5 +0,0 @@
-package com.umg.proyectoAnalasis.controller;
-
-public class JwtAuthenticationController {
-
-}
--- proyectoAnalasis/src/main/java/com/umg/proyectoAnalasis/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.umg.proyectoAnalasis.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-	private String idUsuario;
-    private String password;
-}
