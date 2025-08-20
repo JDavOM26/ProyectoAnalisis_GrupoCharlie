6bb6d4237bc73eee1d89ce971fcb7fc1a50d723c
MAAD03 <mescobarc20@miumg.edu.gt>
2025-08-20 17:15:04 -0600

    Registrar Usuario
    El registro de Usuario y validacion de contrasena segun Empresa al que se registro.

--- ProyectoAnalisis/src/main/java/com/umg/proyectoanalisis/controller/principales/EmpresaController.java
+++ ProyectoAnalisis/src/main/java/com/umg/proyectoanalisis/controller/principales/EmpresaController.java
@@ -1,9 +1,26 @@
 package com.umg.proyectoanalisis.controller.principales;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.umg.proyectoanalisis.entity.principales.Empresa;
+import com.umg.proyectoanalisis.service.EmpresaService;
 
 @RestController
-@RequestMapping("/api/noauth")
+@RequestMapping("/api/noauth/empresa")
 public class EmpresaController {
 
+    @Autowired
+    private EmpresaService empresaService;
+
+    @GetMapping("/{idEmpresa}")
+    public ResponseEntity<Empresa> obtenerEmpresa(@PathVariable int idEmpresa) {
+        try {
+            Empresa empresa = empresaService.findById(idEmpresa);
+            return ResponseEntity.ok(empresa);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
