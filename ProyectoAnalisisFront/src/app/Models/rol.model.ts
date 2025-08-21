0148e447afddc790dd970fbde49805e6968615d4
Ivan200110 <adeleonc19@miumg.edu.gt>
2025-08-21 10:38:44 -0600

    Add files via upload

---
+++ ProyectoAnalisisFront/src/app/Models/rol.model.ts
@@ -0,0 +1,8 @@
+export interface Rol {
+  IdRol: string; // Identificador único del rol
+  Nombre: string; // Nombre del rol
+  FechaCreacion?: string; // Fecha de creación en formato ISO datetime
+  UsuarioCreacion?: string; // Usuario que creó el rol
+  FechaModificacion?: string | null; // Fecha de última modificación en formato ISO datetime
+  UsuarioModificacion?: string | null; // Usuario que modificó el rol
+}
