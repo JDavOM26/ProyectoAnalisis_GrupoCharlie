078643c82806b308c7821f1a1ec02a92b83b877d
Jose David Oliva <jdavidolivamuralles@gmail.com>
2025-08-27 09:31:22 -0600

    Se añadí manejo global de CorsConfigurationSource en la clase WebSecurityConfig

--- ProyectoAnalisis/src/main/java/com/umg/proyectoanalisis/security/WebSecurityConfig.java
+++ ProyectoAnalisis/src/main/java/com/umg/proyectoanalisis/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.umg.proyectoanalisis.security;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,58 +12,66 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.umg.proyectoanalisis.securityservice.JwtUserDetailsService;
-
 
 @Configuration
 public class WebSecurityConfig {
-    
+
     @Autowired
     JwtUserDetailsService userDetailsService;
-    
+
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
-    
+
     @Autowired
-    private JwtAuthTokenFilter jwtAuthTokenFilter; 
-    
-    
+    private JwtAuthTokenFilter jwtAuthTokenFilter;
+
     @Bean
-     AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration
-    ) throws Exception {
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-   
     @Bean
-     PasswordEncoder passwordEncoder() {
-    	return new BCryptPasswordEncoder();
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-    
-   
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       
         http
-            .csrf(csrf -> csrf.disable()) 
-            .cors(cors -> cors.disable()) 
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .exceptionHandling(exceptionHandling ->
                 exceptionHandling.authenticationEntryPoint(unauthorizedHandler)
             )
             .sessionManagement(sessionManagement ->
-            //STATELESS, no guarda información de sesión del usuario en el servidor entre peticiones.
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests
                     .requestMatchers("/api/noauth/**").permitAll()
+                    .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll() // Permitir OPTIONS sin autenticación
                     .anyRequest().authenticated()
             );
-        
-        // Añade JwtAuthTokenFilter antes del filtro de autenticación estándar.
+
         http.addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
