b7f6bef02f7141496f204157239d1a0f4d6f088b
Ivan200110 <adeleonc19@miumg.edu.gt>
2025-10-15 21:59:01 -0600

    Add files via upload

---
+++ ProyectoAnalisisFront/src/app/Models/tipo_documento.model.ts
@@ -0,0 +1,8 @@
+export interface tipo_documento {
+  IdTipoDocumento: string;
+  Nombre: string;
+  FechaCreacion?: string;              // ISO datetime
+  UsuarioCreacion?: string;
+  FechaModificacion?: string | null;   // ISO datetime
+  UsuarioModificacion?: string | null; // username
+}
